fa743636f7e6e4721e5faa63ad88664603eb7fa0
Steven Lu Chen <steven.lu.chen@gmail.com>
2026-07-19 23:08:27 -0400

    added OpenSky ingestion pipeline to Postgres

---
+++ app/db.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import os
+
+load_dotenv()
+
+def get_engine():
+    database_url = (
+        f"postgresql+psycopg2://"
+        f"{os.getenv('POSTGRES_USER')}:"
+        f"{os.getenv('POSTGRES_PASSWORD')}@"
+        f"localhost:{os.getenv('POSTGRES_PORT')}/"
+        f"{os.getenv('POSTGRES_DB')}"
+    )
+
+    return create_engine(database_url)
--- app/ingest.py
+++ app/ingest.py
@@ -1,26 +1,114 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
-import os
+import requests
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy import text
 
-# Load variables from .env
-load_dotenv()
+from db import get_engine
 
-DB_NAME = os.getenv("POSTGRES_DB")
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_PORT = os.getenv("POSTGRES_PORT")
+OPEN_SKY_URL = "https://opensky-network.org/api/states/all"
 
-# When running this script on your host machine,
-# Postgres is accessible via localhost because of port mapping.
-DB_HOST = "localhost"
+def fetch_states():
+    response = requests.get(
+        OPEN_SKY_URL,
+        timeout=30
+    )
+    response.raise_for_status()
+    return response.json()
 
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+def insert_states(data):
+    engine = get_engine()
+    snapshot_id = str(uuid.uuid4())
+    api_time = data["time"]
+    states = data["states"]
+    print(f"Snapshot: {snapshot_id}")
+    print(f"Aircraft found: {len(states)}")
 
-engine = create_engine(DATABASE_URL)
+    insert_query = text("""
+        INSERT INTO raw_opensky_states (
+            snapshot_id,
+            api_time,
+            icao24,
+            callsign,
+            origin_country,
+            time_position,
+            last_contact,
+            longitude,
+            latitude,
+            baro_altitude,
+            on_ground,
+            velocity,
+            true_track,
+            vertical_rate,
+            geo_altitude,
+            squawk,
+            spi,
+            position_source,
+            ingested_at
+        )
+        VALUES (
+            :snapshot_id,
+            :api_time,
+            :icao24,
+            :callsign,
+            :origin_country,
+            :time_position,
+            :last_contact,
+            :longitude,
+            :latitude,
+            :baro_altitude,
+            :on_ground,
+            :velocity,
+            :true_track,
+            :vertical_rate,
+            :geo_altitude,
+            :squawk,
+            :spi,
+            :position_source,
+            :ingested_at
+        )
+    """)
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT version();"))
-    print(result.scalar())
+    rows = []
+
+    for state in states:
+        rows.append({
+            "snapshot_id": snapshot_id,
+            "api_time": api_time,
+
+            "icao24": state[0],
+            "callsign": state[1],
+            "origin_country": state[2],
+
+            "time_position": state[3],
+            "last_contact": state[4],
+
+            "longitude": state[5],
+            "latitude": state[6],
+
+            "baro_altitude": state[7],
+            "on_ground": state[8],
+
+            "velocity": state[9],
+            "true_track": state[10],
+            "vertical_rate": state[11],
+
+            "geo_altitude": state[13],
+
+            "squawk": state[14],
+            "spi": state[15],
+            "position_source": state[16],
+
+            "ingested_at": datetime.now(timezone.utc)
+        })
+
+    with engine.begin() as conn:
+        conn.execute(insert_query, rows)
+
+def main():
+    print("Fetching OpenSky data...")
+    data = fetch_states()
+    insert_states(data)
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
