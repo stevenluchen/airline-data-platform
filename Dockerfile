9a7967a53266b896f4c440d58b5791d77a0190f2
Steven Lu Chen <steven.lu.chen@gmail.com>
2026-08-24 16:45:28 -0700

    convert pipeline from manually run script into dockerized batch pipeline - more Airflow prep

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY app/requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY app/ .
+
+CMD ["python", "ingest_state_vectors.py"]
--- app/db.py
+++ app/db.py
@@ -9,7 +9,8 @@ def get_engine():
         f"postgresql+psycopg2://"
         f"{os.getenv('POSTGRES_USER')}:"
         f"{os.getenv('POSTGRES_PASSWORD')}@"
-        f"localhost:{os.getenv('POSTGRES_PORT')}/"
+        f"{os.getenv('POSTGRES_HOST')}:"
+        f"{os.getenv('POSTGRES_PORT')}/"
         f"{os.getenv('POSTGRES_DB')}"
     )
 
